3ae2ea2487e9554c6d46fe06470a6e01c267171e
Berk Disli <berkdisli@yahoo.com>
2023-04-19 16:02:12 +0200

    upload, multer

--- server/src/routes/admin.js
+++ server/src/routes/admin.js
@@ -1,10 +1,12 @@
-const formidable = require("express-formidable");
 const adminRouter = require("express").Router();
 const session = require("express-session");
 const { isLoggedIn, isLoggedOut } = require("../middlewares/auth");
 const dev = require("../config/index");
 
 const { loginAdmin, logoutAdmin } = require("../controllers/admin");
+const isAdmin = require("../middlewares/isAdmin");
+const { registerUser } = require("../controllers/users");
+const upload = require("../middlewares/upload");
 
 adminRouter.use(
     session({
@@ -17,5 +19,6 @@ adminRouter.use(
 );
 adminRouter.post("/login", isLoggedOut, loginAdmin);
 adminRouter.get("/logout", isLoggedIn, logoutAdmin);
+adminRouter.post("/register", upload.single("image"), registerUser);
 
 module.exports = adminRouter;
